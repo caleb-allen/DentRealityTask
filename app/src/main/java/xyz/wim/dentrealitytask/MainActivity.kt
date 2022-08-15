7e4340fb517ee9204ca9b10211115233f3728ef5
Caleb Allen <mail@caleb.dev>
2022-08-15 13:26:30 -0700

    initial commit

---
+++ app/src/main/java/xyz/wim/dentrealitytask/MainActivity.kt
@@ -0,0 +1,18 @@
+package xyz.wim.dentrealitytask
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import xyz.wim.dentrealitytask.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, MainFragment.newInstance())
+                .commitNow()
+        }
+    }
+}
---
+++ app/src/main/java/xyz/wim/dentrealitytask/ui/main/MainViewModel.kt
@@ -0,0 +1,7 @@
+package xyz.wim.dentrealitytask.ui.main
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
